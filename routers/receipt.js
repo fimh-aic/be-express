842c9f7bf6a7e6a46d21ef29b96874b1f35a5c50
Muhammad Risqi Firdaus <risqi.firdaus20@gmail.com>
2023-09-03 23:40:56 +0700

    add uploader foto at routers/receipt.js

--- routers/receipt.js
+++ routers/receipt.js
@@ -1,10 +1,29 @@
 const express = require('express');
 const router = express.Router();
 const receiptControllers = require('../controllers/receiptControllers');
-
+import multer, { File } from 'multer';
 const path = "/receipts"
 
+const fileFilter = (req, file, cb) => {
+    if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png') {
+        cb(null, true)
+    }
+    else {
+        cb(new Error('File type not supported'), false)
+    }
+}
+
+const multerStorage = multer.memoryStorage();
+const upload = multer({
+    storage: multerStorage,
+    fileFilter: fileFilter
+});
+
+const uploadPhoto = upload.single('photo');
+
+
 router.get(`${path}`, receiptControllers.getAllReceipts);
-router.get(`${path}/:bahan`, receiptControllers.getReceiptByBahanWithConstraints);
+// TO DO: add uploadPhoto as middleware
+router.get(`${path}/`, uploadPhoto, /** ntar di sini manggil controller baut handle upload */);
 
 module.exports = router;
